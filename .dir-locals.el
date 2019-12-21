6016dba3dbe04289839335facff84ca814c42a5a
Sreejith Pp <1743700+ppsreejith@users.noreply.github.com>
2019-12-22 01:47:29 +0530

    Update project settings to use relative directories

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,25 +1,24 @@
-((nil . ((org-publish-project-alist
-          . (("pages"
-              :base-directory "~/projects/org-web/src"
-              :publishing-directory "~/projects/org-web/public"
-              :recursive t
-              :base-extension "org"
-              :exclude ".*-config.org"
-              :publishing-function org-html-publish-to-html)
-
-             ("styles"
-              :base-directory "~/projects/org-web/src/styles"
-              :publishing-directory "~/projects/org-web/public/styles"
-              :recursive t
-              :base-extension "css"
-              :publishing-function org-publish-attachment)
-
-             ("scripts"
-              :base-directory "~/projects/org-web/src/scripts"
-              :publishing-directory "~/projects/org-web/public/scripts"
-              :recursive t
-              :base-extension "js"
-              :publishing-function org-publish-attachment)
-
-             ("project" :components ("pages" "styles" "scripts"))))
+((nil . ((eval . (cl-flet ((path (dir) (concat (projectile-project-root) dir)))
+                   (setq org-publish-project-alist
+                         (list
+                          (list "pages"
+                                :base-directory (path "src")
+                                :publishing-directory (path "public")
+                                :recursive t
+                                :base-extension "org"
+                                :exclude ".*-config.org"
+                                :publishing-function 'org-html-publish-to-html)
+                          (list "styles"
+                                :base-directory (path "src/styles")
+                                :publishing-directory (path "public/styles")
+                                :recursive t
+                                :base-extension "css"
+                                :publishing-function 'org-publish-attachment)
+                          (list "scripts"
+                                :base-directory (path "src/scripts")
+                                :publishing-directory (path "public/scripts")
+                                :recursive t
+                                :base-extension "js"
+                                :publishing-function 'org-publish-attachment)
+                          '("project" :components ("pages" "styles" "scripts"))))))
          )))
