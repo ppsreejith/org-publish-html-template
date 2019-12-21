117b6252fe3f5817ebd4c91e7ddb9d7f6730532a
Sreejith Pp <1743700+ppsreejith@users.noreply.github.com>
2019-12-21 19:59:09 +0530

    Update dir locals to get basic publishing working

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,24 @@
-(setq org-publish-project-alist
-      '(
-        ("org-notes"
-         :base-directory "./blog"
-         :base-extension "org"
-         :publishing-directory "./blog/output"
-         :publishing-function org-html-publish-to-html
-         )
-        )
-      ("blog" :components ("org-notes"))
-      )
+((nil . ((org-publish-project-alist
+          . (("pages"
+              :base-directory "~/projects/org-web/src"
+              :publishing-directory "~/projects/org-web/public"
+              :recursive t
+              :base-extension "org"
+              :publishing-function org-html-publish-to-html)
+
+             ("styles"
+              :base-directory "~/projects/org-web/src"
+              :publishing-directory "~/projects/org-web/public/styles"
+              :recursive t
+              :base-extension "css"
+              :publishing-function org-publish-attachment)
+
+             ("scripts"
+              :base-directory "~/projects/org-web/src"
+              :publishing-directory "~/projects/org-web/public/scripts"
+              :recursive t
+              :base-extension "js"
+              :publishing-function org-publish-attachment)
+
+             ("project" :components ("pages" "styles" "scripts"))))
+         )))
